47d94fbc1df9eb604477c8bd89375859a7d9b1d6
Vajrayanu <yarnu.watchara@gmail.com>
2020-07-08 17:12:36 +0700

    Add files via upload

---
+++ App_Code/EmailSender.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net.Mail;
+using System.Net;
+using System.Globalization;
+using System.Configuration;
+using System.IO;
+using System.Collections;
+using System.Web;
+//using LogManager;
+
+public class EmailPattern
+{
+    public static bool SendMail(string Subject, string reqMailTo, string reqMailCC, string MailBody)
+    {
+        try
+        {
+            string mail_to = "", mail_cc = "", mail_from = "", mail_subject = "", mail_body = "";
+            if (reqMailTo != null && !reqMailTo.Equals(""))
+            {
+                mail_to = reqMailTo;
+            }
+            else
+            {
+                mail_to = "";
+            }
+
+            if (reqMailCC != null && !reqMailCC.Equals(""))
+            {
+                mail_cc = reqMailCC;
+            }
+            else
+            {
+                mail_cc = "";
+            }
+
+            //AppLog.Log("mail_to : " + mail_to);
+            //AppLog.Log("mail_cc : " + mail_cc);
+
+            mail_from = ConfigurationManager.AppSettings["mail_from"].ToString();
+
+            mail_subject = Subject;
+            mail_body = MailBody;
+
+            if (!mail_to.Equals(""))
+            {
+                int sentMailStatus = DoSendEmail(mail_from, mail_cc, mail_to, mail_subject, mail_body);
+                //AppLog.Log("do send mail : " + sentMailStatus);
+            }
+            return true;
+        }
+        catch (Exception ex)
+        {
+            return false;
+        }
+    }
+
+    public static int DoSendEmail(string mail_from, string mail_cc, string mail_to, string mail_subject, string mail_body)
+    {
+        try
+        {
+            bool result = EmailSender.SendMail(mail_from, mail_cc, mail_to, mail_subject, mail_body, true);
+            if (result)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        catch (System.Exception Err)
+        {
+            //AppLog.Log("EmailPattern: DoSendEmail:" + Err.StackTrace + "-->" + Err.Message);
+            return -1;
+        }
+    }
+}
+
+public class EmailSender
+{
+    public static bool SendMail(string mail_from, string mail_cc, string mail_to, string mail_subject, string mail_body, bool isBodyHtml)
+    {
+        try
+        {
+            using (MailMessage mailMessage = new MailMessage())
+            {
+                string mailServer = ConfigurationManager.AppSettings["mailServer"].ToString();
+                int mailServerPort = Convert.ToInt32(ConfigurationManager.AppSettings["mailServerPort"]);
+                string mailUser = ConfigurationManager.AppSettings["mailUser"].ToString();
+                string mailPassword = ConfigurationManager.AppSettings["mailPassword"].ToString();
+                string mail_ssl = ConfigurationManager.AppSettings["mail_ssl"].ToString();
+                //MailAddress addressBCC = new MailAddress("it.dev@wallstreetenglish.in.th , ssds@wallstreetenglish.in.th ");
+
+                //string SentMailTo = SendTo + " jruriders@gmail.com ";
+
+                mailMessage.From = new MailAddress(mail_from);
+
+                //mailMessage.Bcc.Add(addressBCC);
+                if (!mail_cc.Equals(""))
+                {
+                    mailMessage.CC.Add(mail_cc);
+                }
+
+                mailMessage.Subject = mail_subject;
+
+                mailMessage.Body = mail_body;
+
+                mailMessage.IsBodyHtml = true;
+
+                //mailMessage.To.Add(new MailAddress(SendTo));
+                mailMessage.To.Add(mail_to);
+
+
+                SmtpClient mailClient = new SmtpClient(mailServer, mailServerPort);
+
+                //mailClient.Host = ConfigurationManager.AppSettings["Host"];
+                mailClient.Credentials = new NetworkCredential(mailUser, mailPassword);
+
+                if (mail_ssl.ToLower().Equals("true"))
+                {
+                    mailClient.EnableSsl = true;
+                }
+                else
+                {
+                    mailClient.EnableSsl = false;
+                }
+
+
+                mailClient.Send(mailMessage);
+                return true;
+            }
+        }
+        catch (Exception ex)
+        {
+            //AppLog.Log("EmailSender:SendMail Err:" + ex.StackTrace + "-->" + ex.Message);
+            return false;
+        }
+    }
+
+}
---
+++ App_Code/includefunction.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+using System.Collections;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class includefunction
+{
+    public DataTable Getcenter(SqlConnection db)
+    {
+        StringBuilder sb = new StringBuilder();
+        sb.AppendLine(" SELECT wsec_name,  ");
+        sb.AppendLine(" wsec_llmscenterid, ");
+        sb.AppendLine(" wsec_avaya, ");
+        sb.AppendLine(" wsec_TerritoryID, ");
+        sb.AppendLine(" WSEC_CenterID ");
+        sb.AppendLine(" FROM [wsi-cluster].CRM.dbo.WSECenter as ce ");
+        sb.AppendLine(" WHERE 1=1  ");
+        sb.AppendLine(" and ce.wsec_disable = 'enable' ");
+
+        SqlCommand sqlcenter = new SqlCommand(sb.ToString(), db);
+
+        DataTable tablecenter = new DataTable();
+        tablecenter.Load(sqlcenter.ExecuteReader());
+
+        return tablecenter;
+    }
+
+    public DataTable GetTimeSlot(SqlConnection db, string timetype)
+    {
+        StringBuilder sb = new StringBuilder();
+        sb.AppendLine(" SELECT ts.Time_slot, ");
+        sb.AppendLine(" ts.slotId ");
+        sb.AppendLine(" FROM Time_slot as ts ");
+        sb.AppendLine(" WHERE 1=1 ");
+
+        if (timetype.Equals("start"))
+        {
+            sb.AppendLine(" and Time_slot between '09' and '19' ");
+        }
+        else
+        {
+            sb.AppendLine(" and Time_slot between '11' and '20' ");
+        }
+
+        SqlCommand sql = new SqlCommand(sb.ToString(), db);
+
+        DataTable table = new DataTable();
+        table.Load(sql.ExecuteReader());
+
+        return table;
+    }
+
+    public bool CheckTeacherNxt(string SelectCenterID, string SelectTime, string ClassTime, string ClassCenter)
+    {
+        bool result = false;
+
+        if (SelectCenterID.Equals(ClassCenter))
+        {
+            DateTime SelectTime_ = Convert.ToDateTime("2020-01-27 " + SelectTime).AddHours(1);
+            DateTime ClassTime_ = Convert.ToDateTime("2020-01-27 " + ClassTime);
+
+            if (SelectTime_ <= ClassTime_)
+            {
+                result = true;
+            }
+        }
+        else
+        {
+            DateTime SelectTime_ = Convert.ToDateTime("2020-01-27 " + SelectTime).AddHours(2);
+            DateTime ClassTime_ = Convert.ToDateTime("2020-01-27 " + ClassTime);
+
+            if (SelectTime_ <= ClassTime_)
+            {
+                result = true;
+            }
+        }
+
+        return result;
+    }
+
+    public bool CheckTeacherPrv(string SelectCenterID, string SelectTime, string ClassTime, string ClassCenter)
+    {
+        bool result = false;
+
+        if (SelectCenterID.Equals(ClassCenter))
+        {
+            DateTime SelectTime_ = Convert.ToDateTime("2020-01-27 " + SelectTime);
+            DateTime ClassTime_ = Convert.ToDateTime("2020-01-27 " + ClassTime).AddHours(1);
+
+            if (SelectTime_ >= ClassTime_)
+            {
+                result = true;
+            }
+        }
+        else
+        {
+            DateTime SelectTime_ = Convert.ToDateTime("2020-01-27 " + SelectTime);
+            DateTime ClassTime_ = Convert.ToDateTime("2020-01-27 " + ClassTime).AddHours(2);
+
+            if (SelectTime_ >= ClassTime_)
+            {
+                result = true;
+            }
+        }
+
+        return result;
+    }
+
+    public string EncryptData(string KeyPassword, string Data)
+    {
+        string original, password;
+        TripleDESCryptoServiceProvider des;
+        MD5CryptoServiceProvider hashmd5;
+        byte[] pwdhash, buff;
+
+        password = KeyPassword;
+        original = Data;
+        hashmd5 = new MD5CryptoServiceProvider();
+        pwdhash = hashmd5.ComputeHash(ASCIIEncoding.ASCII.GetBytes(password));
+        hashmd5 = null;
+        des = new TripleDESCryptoServiceProvider();
+        des.Key = pwdhash;
+        des.Mode = CipherMode.ECB;
+
+        buff = ASCIIEncoding.ASCII.GetBytes(original);
+        return Convert.ToBase64String(des.CreateEncryptor().TransformFinalBlock(buff, 0, buff.Length));
+    }
+
+    public string DecryptData(string strKeyPassword, string Data)
+    {
+        try
+        {
+            if (Data.Trim().Length > 0)
+            {
+                string original, password;
+                TripleDESCryptoServiceProvider des;
+                MD5CryptoServiceProvider hashmd5;
+                byte[] pwdhash, buff;
+
+                password = strKeyPassword;
+
+                original = Data;
+                original = original.Replace(" ", "+");
+
+                int mod4 = original.Length % 4;
+                if (mod4 > 0)
+                {
+                    original += new string('=', 4 - mod4);
+                }
+
+                hashmd5 = new MD5CryptoServiceProvider();
+                pwdhash = hashmd5.ComputeHash(ASCIIEncoding.ASCII.GetBytes(password));
+                hashmd5 = null;
+
+                des = new TripleDESCryptoServiceProvider();
+                des.Key = pwdhash;
+                des.Mode = CipherMode.ECB;
+                //----- decrypt an encrypted string ------------
+                //whenever you decrypt a string, you must do everything you
+                //did to encrypt the string, but in reverse order. To encrypt,
+                //first a normal string was des3 encrypted into a byte array
+                //and then base64 encoded for reliable transmission. So, to 
+                //decrypt this string, first the base64 encoded string must be 
+                //decoded so that just the encrypted byte array remains.
+                buff = Convert.FromBase64String(original);
+
+                //decrypt DES 3 encrypted byte buffer and return ASCII string
+                return ASCIIEncoding.ASCII.GetString(des.CreateDecryptor().TransformFinalBlock(buff, 0, buff.Length));
+            }
+            else
+            {
+                return "";
+            }
+        }
+        catch (Exception decryption)
+        {
+            return "";
+        }
+    }
+
+    public string GetEmailByCookieOrSession()
+    {
+        HttpContext context = HttpContext.Current;
+
+        string Email = "";
+        try
+        {
+            List<string> _Userinfo = new List<string>();
+            _Userinfo = (List<string>)context.Session["Userinfo"];
+
+            if (context.Session["Userinfo"] != null)
+            {
+                Email = _Userinfo[1].ToString();
+            }
+            else
+            {
+            }
+        }
+        catch (Exception ex)
+        {
+        }
+
+        return Email;
+    }
+}
